03d2e982c4f85b85fa8ba182f6e0371bfcf3dc62
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-06-17 13:57:50 +0100

    route to another page/goodbye

--- workshop/server.js
+++ workshop/server.js
@@ -3,9 +3,15 @@ const PORT = 3000;
 
 //Why does the network tab not show any details of any requests?
 const server = http.createServer((request, response) => {
-  console.log(request.method, request.url);
-  response.writeHead(200, { "content-type": "text/html" });
-  response.end("<h1>Hello</h1>");
+  //Where does the request corresponding to favicon,ico come from?
+  const url = request.url;
+  if (url === "/") {
+    response.writeHead(200, { "content-type": "text/html" });
+    response.end("<h1>Hello</h1>");
+  } else if (url === "/goodbye") {
+    response.writeHead(200, { "content-type": "text/html" });
+    response.end("<h1>Goodbye</h1>");
+  }
 });
 
 server.listen(PORT, () => console.log(`Listen on http://localhost:${PORT}`));
