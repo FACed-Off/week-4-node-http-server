13c7718f1cafdb583290883ec39fdf4c5e1c90c6
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-06-17 13:40:29 +0100

    feat: add html body

--- workshop/server.js
+++ workshop/server.js
@@ -4,8 +4,8 @@ const PORT = 3000;
 //Why does the network tab not show any details of any requests?
 const server = http.createServer((request, response) => {
   response.statusCode = 200;
-  response.setHeader("content-type", "text/plain");
-  response.end("hello");
+  response.setHeader("content-type", "text/html");
+  response.end("<h1>Hello</h1>");
 });
 
 server.listen(PORT, () => console.log(`Listen on http://localhost:${PORT}`));
