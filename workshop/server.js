aa1b930ff025ddcf26eb410f460fd67908eb4d88
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-06-17 13:01:24 +0100

    feat: add listen method to server object

--- workshop/server.js
+++ workshop/server.js
@@ -1,3 +1,8 @@
 const http = require("http");
+const PORT = 3000;
 
-const server = http.createServer();
+const server = http.createServer((request, response) => {
+  response.end("hello");
+});
+
+server.listen(PORT, () => console.log(`Listen on http://localhost:${PORT}`));
