684d5609ae80487cdde9cc5d395102e1935aeeda
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-06-17 13:50:34 +0100

    add query about favicon request

--- workshop/server.js
+++ workshop/server.js
@@ -3,6 +3,7 @@ const PORT = 3000;
 
 //Why does the network tab not show any details of any requests?
 const server = http.createServer((request, response) => {
+  console.log(request.method, request.url);
   response.writeHead(200, { "content-type": "text/html" });
   response.end("<h1>Hello</h1>");
 });
