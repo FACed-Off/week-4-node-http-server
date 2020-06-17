cfc507642bcd673a22a0fbf7927bf8ce09e829de
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-06-17 13:33:29 +0100

    feat: add status code and header

--- workshop/server.js
+++ workshop/server.js
@@ -1,7 +1,10 @@
 const http = require("http");
 const PORT = 3000;
 
+//Why does the network tab not show any details of any requests?
 const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader("content-type", "text/plain");
   response.end("hello");
 });
 
