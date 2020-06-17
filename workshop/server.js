263505401f4cc00859df9f31c3efc7a3ae1fc5e3
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-06-17 13:42:39 +0100

    feat: use writeHead method to set status code and header together

--- workshop/server.js
+++ workshop/server.js
@@ -3,8 +3,7 @@ const PORT = 3000;
 
 //Why does the network tab not show any details of any requests?
 const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader("content-type", "text/html");
+  response.writeHead(200, { "content-type": "text/html" });
   response.end("<h1>Hello</h1>");
 });
 
