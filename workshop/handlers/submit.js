ff8b30829c3a8096016b6057c5ba1a90a6a6998c
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-06-18 13:34:45 +0100

    feat: add request.on method to call data

--- workshop/handlers/submit.js
+++ workshop/handlers/submit.js
@@ -1,6 +1,15 @@
 function submitHandler(request, response) {
-  response.writeHead(200, { "content-type": "text/html" });
-  response.end("<h1>Thank you for submitting</h1>");
+  let body = "";
+  request.on("data", (chunk) => {
+    body += chunk;
+    //With what request will this console.log be called multiple times?
+    console.log(chunk);
+  });
+  request.on("end", () => {
+    console.log("End:" + body);
+    response.writeHead(200, { "content-type": "text/html" });
+    response.end("<h1>Thank you for submitting</h1>");
+  });
 }
 
 module.exports = submitHandler;
