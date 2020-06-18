26371ad8ca2d71d1230cad97aa616a44b0d49e40
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-06-18 11:06:13 +0100

    feat: add submit handling to router

--- workshop/router.js
+++ workshop/router.js
@@ -2,15 +2,19 @@ const homeHandler = require("./handlers/home");
 const goodbyeHandler = require("./handlers/goodbye");
 const helloHandler = require("./handlers/hello");
 const missingHandler = require("./handlers/missing");
+const submitHandler = require("./handlers/submit");
 
 function router(request, response) {
   const url = request.url;
+  const method = request.method;
   if (url === "/") {
     homeHandler(request, response);
   } else if (url === "/goodbye") {
     goodbyeHandler(request, response);
   } else if (url === "/hello") {
     helloHandler(request, response);
+  } else if (url === "/submit" && method === "POST") {
+    submitHandler(request, response);
   } else {
     missingHandler(request, response);
   }
