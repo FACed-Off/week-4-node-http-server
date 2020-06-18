9c1b7ceec1c7e739903ff831718dc3f486bce361
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-06-18 14:41:48 +0100

    feat: add error request and dynamic response

--- workshop/handlers/submit.js
+++ workshop/handlers/submit.js
@@ -1,14 +1,24 @@
+const { URLSearchParams } = require("url");
+
 function submitHandler(request, response) {
   let body = "";
   request.on("data", (chunk) => {
     body += chunk;
     //With what request will this console.log be called multiple times?
     console.log(chunk);
+    //response.send("Error", 500);
   });
   request.on("end", () => {
-    console.log("End:" + body);
+    //
+    const data = new URLSearchParams(body);
+    const name = data.get("name");
+    console.log(name);
     response.writeHead(200, { "content-type": "text/html" });
-    response.end("<h1>Thank you for submitting</h1>");
+    response.end(`<h1>Hello${name}</h1>`);
+  });
+  request.on("error", (error) => {
+    response.writeHead(500, { "content-type": "text/html" });
+    response.end(`<h1>${error}</h1>`);
   });
 }
 
