d074b999a0769a7d59f5c950d51d398fd4f38876
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-06-19 13:53:06 +0100

    provide support for server development

--- workshop/handlers/submit.js
+++ workshop/handlers/submit.js
@@ -1,20 +1,24 @@
-const { URLSearchParams } = require("url");
+const { URLSearchParams, URL } = require("url");
 
 function submitHandler(request, response) {
   let body = "";
   request.on("data", (chunk) => {
     body += chunk;
     //With what request will this console.log be called multiple times?
-    console.log(chunk);
+    // console.log(chunk);
     //response.send("Error", 500);
   });
   request.on("end", () => {
-    //
+    // console.log("url ", request.url);
+    // const url = new URL(request.url, "http://localhost:3000");
+    // const searchParams = url.searchParams;
+    // console.log("sp:", searchParams);
+    // const data = new URLSearchParams(searchParams);
     const data = new URLSearchParams(body);
     const name = data.get("name");
-    console.log(name);
+    console.log("name:", name);
     response.writeHead(200, { "content-type": "text/html" });
-    response.end(`<h1>Hello${name}</h1>`);
+    response.end(`<h1>Hello ${name}</h1>`);
   });
   request.on("error", (error) => {
     response.writeHead(500, { "content-type": "text/html" });
