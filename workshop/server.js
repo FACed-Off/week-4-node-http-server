73a11f9fb7a6b371efefffd103d92de2e9eeced9
Meeran B <v9@MacBook-Air.local>
2020-06-22 13:46:30 +0100

    call server listen method

--- workshop/server.js
+++ workshop/server.js
@@ -1,5 +1,8 @@
 const http = require("http");
+const PORT = 3000;
 
 const server = http.createServer((request, response) => {
     response.end("hello");
 });
+
+server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
